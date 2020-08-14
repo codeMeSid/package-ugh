9387fa2f01d8afdc98687baa1d3f022e8107f52e
codemesid <siddhanthbajoria@gmail.com>
2020-08-14 20:30:24 +0530

    publish

---
+++ src/utils/isValidDob.ts
@@ -0,0 +1,17 @@
+import { BadRequestError } from "../errors/bad-request-error";
+
+export const isValidDob = (dob: string | Date) => {
+  const msIn1Sec = 1000;
+  const secIn1Min = msIn1Sec * 60;
+  const minIn1Hr = 60 * secIn1Min;
+  const hrsIn1Day = 24 * minIn1Hr;
+  const daysIn1Year = 365 * hrsIn1Day;
+
+  const sd = new Date(dob).valueOf();
+  const cd = new Date().valueOf();
+
+  if (cd - sd < daysIn1Year * 13) {
+    throw new BadRequestError("Minimum age requirement 13 years");
+  }
+  return true;
+};
