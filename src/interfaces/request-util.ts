f4f961855fcd3bc2acbb190e4e67e01e178844f1
codemesid <siddhanthbajoria@gmail.com>
2020-08-29 20:30:26 +0530

    publish

--- src/interfaces/request-util.ts
+++ src/interfaces/request-util.ts
@@ -4,6 +4,6 @@ export interface RequestUtil {
   url: string;
   method: HttpMethod;
   body: Object;
-  onSuccess?: (data: any) => void;
-  onError?: (errors: Array<{ message: string }>) => void;
+  onSuccess?: (data: any) => any;
+  onError?: (errors: Array<{ message: string }>) => any;
 }
