d8e8acb88cacbb3465847482e1b138abea087c4a
codemesid <siddhanthbajoria@gmail.com>
2020-08-29 21:42:36 +0530

    publish

--- src/interfaces/request-util.ts
+++ src/interfaces/request-util.ts
@@ -2,8 +2,8 @@ import { HttpMethod } from "../enums/http-methods";
 
 export interface RequestUtil {
   url: string;
-  method: HttpMethod;
-  body: Object;
+  method: string;
+  body: any;
   onSuccess?: (data: any) => any;
   onError?: (errors: Array<{ message: string }>) => any;
 }
